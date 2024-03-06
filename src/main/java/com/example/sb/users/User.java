1ba35c924d411b56959b0e42bc5bdff507f8e2d4
areumJeong <add5833@naver.com>
2024-03-06 15:23:06 +0900

    Day 2

--- src/main/java/com/example/sb/users/User.java
+++ src/main/java/com/example/sb/users/User.java
@@ -9,7 +9,14 @@ public class User {
 	private String email;
 	private LocalDate regDate;
 	private int isDeleted;
+	
 	public User() { }
+	public User(String uid, String pwd, String uname, String email) {
+		this.uid = uid;
+		this.pwd = pwd;
+		this.uname = uname;
+		this.email = email;
+	}
 	public User(String uid, String pwd, String uname, String email, LocalDate regDate, int isDeleted) {
 		this.uid = uid;
 		this.pwd = pwd;
